8e75609190046f2ef8b3889e8109f198ac2afbe9
unaimeds <versioncontrol.undamaged050@passinbox.com>
2026-04-16 02:10:01 +0200

    wip: dynamic box size from longest text line

--- src/print.rs
+++ src/print.rs
@@ -1,93 +1,195 @@
 use std::collections::HashMap;
 
 use chrono_humanize::HumanTime;
-use colored::Colorize;
 
 use crate::{
     cache::Cache,
     dto::{PersonalBest, TestResult, UserStats},
 };
 
-// TODO: dynamic padding based on content length
 // TODO: dynamic output size based on terminal's size
 
 const BANNER_SEPARATOR: &str = "·";
+const PB_COLUMNS: &[&str] = &["mode", "wpm", "raw", "acc", "cons"];
 
 pub fn print_user_data(cache: Cache) {
-    banner(&cache.username, cache.user_stats);
+    let (b_text, b_len) = banner(&cache.username, cache.user_stats);
+    let (pb_lines, pb_len) = personal_bests(cache.personal_bests);
+    let (rt_lines, rt_len) = recent_tests(&cache.recent_tests);
+
+    // TODO: round the width to nearest number dividable by 2
+    dbg!(b_len, pb_len, rt_len);
+    // let max_width = [b_len, pb_len, rt_len].into_iter().max().unwrap();
+    let max_width = 50;
+
+    print_banner(&b_text, max_width);
     println!("");
-    personal_bests(cache.personal_bests);
+    print_personal_bests(&pb_lines, max_width);
     println!("");
-    recent_tests(&cache.recent_tests);
+    print_recent_tests(&rt_lines, max_width);
 }
 
-fn banner(username: &str, stats: UserStats) {
+fn banner(username: &str, stats: UserStats) -> (String, usize) {
     let total_secs = stats.time_typing as u32;
     let hours = total_secs / 3600;
     let minutes = (total_secs % 3600) / 60;
     let seconds = total_secs % 60;
 
     let text = format!(
-        "{} {BANNER_SEPARATOR} {} tests {BANNER_SEPARATOR} {hours:02}:{minutes:02}:{seconds:02} typing",
-        username.bright_cyan(),
+        "{username} {BANNER_SEPARATOR} {} tests {BANNER_SEPARATOR} {hours:02}:{minutes:02}:{seconds:02} typing",
         stats.completed_tests,
     );
-    let text_len = text.chars().count();
 
-    print!("{}", "╔".yellow());
-    for _ in 2..text_len {
-        print!("{}", "═".yellow());
-    }
-    println!("{}", "╗".yellow());
-
-    println!(
-        "{0} {text:>pad$} {0:>pad2$}",
-        "║".yellow(),
-        pad = text_len + 2,
-        pad2 = 4,
-    );
-
-    print!("{}", "╚".yellow());
-    for _ in 2..text_len {
-        print!("{}", "═".yellow());
-    }
-    println!("{}", "╝".yellow());
+    let width = text.chars().count();
+    (text, width)
 }
 
-fn personal_bests(bests: HashMap<String, PersonalBest>) {
-    println!("personal bests");
+fn print_banner(inner: &str, width: usize) {
+    let inner_width = width - 4; // padding of 2 whitespaces on each side
+    let padding = inner_width - inner.chars().count();
+
+    let left_pad = padding / 2;
+    let right_pad = padding - left_pad;
+
+    println!("╔{}╗", "═".repeat(width));
+    println!("║  {:left_pad$}{inner}{:right_pad$}  ║", "", "");
+    println!("╚{}╝", "═".repeat(width));
+}
+
+fn personal_bests(bests: HashMap<String, PersonalBest>) -> (Vec<Vec<String>>, usize) {
+    let mut lines = Vec::new();
+
+    let header_line = format!(" {} ", PB_COLUMNS.join(" │ "));
+    let mut max_width = header_line.chars().count();
 
     // TODO: order by mode (15s -> 120s)
     // TODO: add colored output
     // TODO: always show 2 decimal digits in acc & cons
-    println!("┌────────┬──────────┬──────────┬──────────┬───────────┐");
-    println!("│  mode  │    wpm   │    raw   │   acc    │    cons   │");
-    println!("├────────┼──────────┼──────────┼──────────┼───────────┤");
     for (mode, best) in bests {
-        println!(
-            "│  {mode}s   │    {}   │    {}   │  {}%  │   {}%  │",
-            best.wpm.round(),
-            best.raw.round(),
-            best.accuracy,
-            best.consistency,
+        let columns = vec![
+            format!("{mode}s"),
+            format!("{}", best.wpm.round()),
+            format!("{}", best.raw.round()),
+            format!("{}%", best.accuracy),
+            format!("{}%", best.consistency),
+        ];
+
+        let min_line = format!(" {} ", columns.join(" │ "));
+        let width = min_line.chars().count();
+        if width > max_width {
+            max_width = width;
+        }
+
+        lines.push(columns);
+    }
+
+    (lines, max_width)
+}
+
+fn print_cond_border(
+    width: usize,
+    header_width: usize,
+    start: &str,
+    end: &str,
+    normal: &str,
+    on_cond: &str,
+) {
+    print!("{start}");
+    let mut count = 1;
+    for _ in 1..width {
+        if count == header_width {
+            print!("{on_cond}");
+            count = 1;
+        } else {
+            print!("{normal}");
+            count += 1;
+        }
+    }
+    println!("{end}");
+}
+
+fn print_personal_bests(lines: &[Vec<String>], width: usize) {
+    println!("personal bests");
+
+    // calculate how many chars we have for each header
+    let header_width = width / PB_COLUMNS.len();
+
+    // top border
+    // we print additional horizontal border because last header
+    // has one more padding on right due to not having vertical border
+    print_cond_border(width, header_width, "┌", "─┐", "─", "┬");
+
+    // headers
+    print!("│");
+    for (i, col) in PB_COLUMNS.iter().enumerate() {
+        let with_sep = i < PB_COLUMNS.len() - 1;
+
+        let padding = header_width - col.chars().count();
+        let left_pad = padding / 2;
+        // -1 because we add a separator at the end of header
+        let right_pad = padding - left_pad - if with_sep { 1 } else { 0 };
+
+        print!(
+            "{:left_pad$}{col}{:right_pad$}{}",
+            "",
+            "",
+            if with_sep { "│" } else { "" }
         );
     }
-    println!("└────────┴──────────┴──────────┴──────────┴───────────┘");
+    println!("│");
+
+    // bottom header border
+    // same reason for additional horizontal border at end as in top border
+    print_cond_border(width, header_width, "├", "─┤", "─", "┼");
 }
 
 // TODO: add colored output
-fn recent_tests(tests: &[TestResult]) {
-    println!("recent tests");
+fn recent_tests(tests: &[TestResult]) -> (Vec<(String, String)>, usize) {
+    let mut lines = Vec::new();
+    let mut max_width = 0;
+
+    // println!("recent tests");
     println!("────────────────────────────────────────────────────────");
     for test in tests {
-        let when = HumanTime::from(test.timestamp);
-        println!(
-            "▸ {} wpm  raw {}  {}%  {}/{}          {when}",
+        let left = format!(
+            "▸ {} wpm  raw {}  {}%  {}/{}",
             test.wpm.round(),
             test.raw_wpm.round(),
             test.accuracy.round(),
             test.mode,
             test.mode2,
-        )
+        );
+        let when = HumanTime::from(test.timestamp);
+        let right = format!("{when}");
+
+        let min_line = format!("{left}    {right}");
+
+        let width = min_line.chars().count();
+        if width > max_width {
+            max_width = width;
+        }
+
+        lines.push((left, right));
+    }
+
+    (lines, max_width)
+}
+
+fn print_recent_tests(tests: &[(String, String)], width: usize) {
+    let w = width + 2;
+
+    println!("recent tests");
+    println!("{}", "─".repeat(w));
+
+    let max_right_width = tests
+        .iter()
+        .map(|t| &t.1)
+        .map(|s| s.chars().count())
+        .max()
+        .unwrap();
+
+    for (left, right) in tests {
+        let right_pad = w - left.chars().count() - max_right_width;
+        println!("{left}{:right_pad$}{right}", "");
     }
 }
